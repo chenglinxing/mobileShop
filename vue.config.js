1d2ffa38caca374ac165b9a7ec688035d881e7f0
chenglinxing <54135511+chenglinxing@users.noreply.github.com>
2022-10-28 11:03:41 +0800

    修改publicPath（/dailylife改成/dailylife/）

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/dailylife' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/dailylife/' : '/',
     chainWebpack: config => {
         config.resolve.alias
             .set("@", resolve("src"))
