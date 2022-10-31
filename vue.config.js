fbdba82608739fce76e443fdeea8d405822a8e9c
Tiamohub <1428054498@qq.com>
2022-10-31 14:29:33 +0800

    test06

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ function resolve(dir) {
 }
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/dailylife" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/dailylife/" : "/",
   // publicPath: "/",
   chainWebpack: (config) => {
     config.resolve.alias
